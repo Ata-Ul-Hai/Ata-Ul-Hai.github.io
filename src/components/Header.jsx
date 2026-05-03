deb694f454c92ee25c006293d2efe918c9ed913d
Ata-Ul-Hai <ataulhai2007@gmail.com>
2026-05-03 11:12:43 +0530

    feat: replace SVG logo with text signature logo using Mr De Haviland font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,10 +16,7 @@ function Header() {
     <header className={`header${scrolled ? ' scrolled' : ''}`}>
       <div className="container header-content">
         <a href="#hero" className="logo" onClick={closeMenu}>
-          <svg width="40" height="40" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="Ata Ul Hai Signature">
-            <path d="M20 70C25 50 35 30 50 20C65 10 80 20 75 40C70 60 50 80 30 75C10 70 15 50 25 40" stroke="white" strokeWidth="3" strokeLinecap="round"/>
-            <path d="M45 45L65 65M65 45L45 65" stroke="white" strokeWidth="2" strokeLinecap="round"/>
-          </svg>
+          Ata Ul Hai
         </a>
 
         <nav>
